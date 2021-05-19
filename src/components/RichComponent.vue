0eb0ddecd5c9291ba359c7d266de0179d94e1dbd
IrdiEthemi <58038002+IrdiEthemi@users.noreply.github.com>
2021-05-19 16:44:12 +0200

    Update RichComponent.vue

--- src/components/RichComponent.vue
+++ src/components/RichComponent.vue
@@ -20,7 +20,7 @@
 
     @for $i from 1 through 25
         &:nth-child(#{$i}n)
-            animation-delay: #{$i * .80}s
+            animation-delay: #{$i * 1}s
 
     @media screen and (max-width: 720px)
         width: 100%
