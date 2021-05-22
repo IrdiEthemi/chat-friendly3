bf4270d31289e8f0ea499b7267243ece07259005
IrdiEthemi <58038002+IrdiEthemi@users.noreply.github.com>
2021-05-22 20:28:02 +0200

    Update RichComponent.vue

--- src/components/RichComponent.vue
+++ src/components/RichComponent.vue
@@ -20,7 +20,7 @@
 
     @for $i from 1 through 25
         &:nth-child(#{$i}n)
-            animation-delay: #{$i * 3}s
+            animation-delay: #{$i * 1}s // Animation delay lengthened for the bubble animation
 
     @media screen and (max-width: 720px)
         width: 100%
